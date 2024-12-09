124c356fabdd92e29c9e5b5dfbc38e52bb8759bf
Potoroaca Mihai <mihaipoto@gmail.com>
2024-12-09 18:39:33 +0200

    dd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Management;
+
+
+
+var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_SecuritySetting");
+foreach (ManagementObject obj in searcher.Get())
+{
+	Console.WriteLine($"Name: {obj["Caption"]}");
+}
+
